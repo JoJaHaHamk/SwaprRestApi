ca81bb91ff47a510b117f4e0ee331026008dba47
Jorn Van Dijck <jorn.van.dijck@gmail.com>
2023-09-20 23:37:14 +0300

    auto testing?

---
+++ __tests__/classes/User.test.ts
@@ -0,0 +1,5 @@
+describe("User class", () => {
+    test("math test", () => {
+        expect(1 + 1).toBe(2);
+    });
+});
--- __tests__/indes.test.ts
+++
@@ -1,5 +0,0 @@
-describe("Server.ts tests", () => {
-    test("Math test", () => {
-        expect(1 + 1).toBe(2);
-    });
-});
