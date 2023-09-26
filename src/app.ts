50422e830d9495ebeebae5cbe2d85d4ce7e6d1cf
Jorn Van Dijck <jorn.van.dijck@gmail.com>
2023-09-26 14:19:30 +0300

    add book endpoint completed

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 
-import { userRoutes } from "./routes/index";
+import { userRoutes, bookRoutes } from "./routes/index";
 
 const app: Application = express();
 
 app.use(express.json());
 
 app.use("/", userRoutes);
+app.use("/user/:userId/book", bookRoutes);
 
 export default app;
