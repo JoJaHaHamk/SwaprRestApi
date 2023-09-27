772efa7f20ab6da37498ad25dd9053c905e63f08
Jorn Van Dijck <jorn.van.dijck@gmail.com>
2023-09-27 09:28:53 +0300

    progress

--- src/routes/swap.routes.ts
+++ src/routes/swap.routes.ts
@@ -5,8 +5,6 @@ import { states } from "../entity/index";
 
 const router = Router({mergeParams: true});
 
-const bookRepository = AppDataSource.getRepository("Book");
-const userRepository = AppDataSource.getRepository("User");
 const swapRepository = AppDataSource.getRepository("Swap");
 
 // GET /user/:userId/swap
@@ -36,13 +34,6 @@ router.get("/", authMiddleware, async (req: Request, res: Response) => {
 }
 */
 
-/*
-    How must this work? The bookstate of the user that accepts the swap must be changed to "accepted". 
-    The bookstate of the user that proposed the swap must be changed to "pending".
-    How do we let the user know that the swap has been accepted?
-    Do we use the actual tinder system where both users have to swipe right or 
-    does one user swipe right and the other one gets a notification?
-*/
 router.patch("/:swapId", authMiddleware, async (req: Request, res: Response) => {
     // Get all the fields from the body for easier access
     const body = req.body;
